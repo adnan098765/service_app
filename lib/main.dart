f7b8cf3f47be9afbde3e8ae58892f599148b00f3
Adnan Qasim <adnanqasim804@gmail.com>
2025-07-31 22:39:49 +0500

    initial changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:untitled2/screens/Controlller/add_address_controller.dart';
 
 import 'splash/splash_screen.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init();
-  runApp(MyApp());
+  await Get.putAsync(() async {
+    final controller = AddAddressController();
+    print(
+      'main.dart: AddAddressController registered, hashCode: ${controller.hashCode} @ ${DateTime.now().toIso8601String()}',
+    );
+    await controller.loadSavedAddresses(); // Ensure initialization completes
+    return controller;
+  }, permanent: true);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +26,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ResponsiveSizer(
       builder: (context, orientation, screenType) {
-        return GetMaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SplashScreen(),
+        );
       },
     );
   }
